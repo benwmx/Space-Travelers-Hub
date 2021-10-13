feba30a540bca0ccfb722a9ef6bcca31b3a1a08e
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-13 13:59:25 +0100

    Add style to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../img/logo.svg';
+import Styles from './header.module.scss';
 
 const Header = () => (
-  <header>
-    <div className="logo">
+  <header className={Styles.header}>
+    <div className={Styles.logo}>
       <img src={logo} alt="Yellow Planet" style={{ width: '50px', height: '50px' }} />
-      <h1>Space Travelers&apos; Hub</h1>
+      <h1 className={Styles.title}>Space Travelers&apos; Hub</h1>
     </div>
-    <div className="links">
-      <NavLink to="/" activeClassName="" exact>
+    <div className={Styles.navLinks}>
+      <NavLink to="/" activeClassName={Styles.activeLink} exact>
         Rockets
       </NavLink>
-      <NavLink to="/missions" activeClassName="" exact>
+      <NavLink to="/missions" activeClassName={Styles.activeLink} exact>
         Missions
       </NavLink>
-      <NavLink to="/myprofile" activeClassName="" exact>
+      <NavLink to="/myprofile" activeClassName={Styles.activeLink} exact>
         My profile
       </NavLink>
     </div>
