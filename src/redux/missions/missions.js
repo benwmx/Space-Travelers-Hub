8cc87f2485d3b93039171233aa2161135cb5ee57
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-13 07:26:12 +0100

    Add redux-thunk for getting Data from API.

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,5 +1,7 @@
 const TOGGLE_MESSION_RESERVATION = 'TOGGLE_MESSION_RESERVATION';
+const ADD_LIST_OF_MISSIONS_TO_STORE = 'ADD_LIST_OF_MISSIONS_TO_STORE';
 
+const url = 'https://api.spacexdata.com/v3/missions';
 const initialState = [];
 
 const toggleMissionReservation = (payload) => ({
@@ -7,18 +9,40 @@ const toggleMissionReservation = (payload) => ({
   payload,
 });
 
+const addListOfMissionsToStore = (payload) => ({
+  type: ADD_LIST_OF_MISSIONS_TO_STORE,
+  payload,
+});
+
+const filterMissionsData = (data) => data.map((mission) => ({
+  id: mission.mission_id,
+  name: mission.mission_name,
+  description: mission.description,
+}));
+
+// redux thunks middlewares for adding/getting/removing books from the API.
+const getListOfMissions = () => (dispatch) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const missions = filterMissionsData(data);
+      dispatch(addListOfMissionsToStore(missions));
+    });
+};
+//
 const missionsReducer = (state = initialState, action) => {
   switch (state.type) {
     case TOGGLE_MESSION_RESERVATION: return state.map((mission) => {
-      let newMission = mission;
-      if (mission.id === action.payload) newMission = { ...mission, reserved: !mission.reserved };
-      return newMission;
+      if (mission.id !== action.payload) return mission;
+      return { ...mission, reserved: !mission.reserved };
     });
+    case ADD_LIST_OF_MISSIONS_TO_STORE: return action.payload;
     default: return state;
   }
 };
 
 export {
   toggleMissionReservation,
+  getListOfMissions,
   missionsReducer as default,
 };
